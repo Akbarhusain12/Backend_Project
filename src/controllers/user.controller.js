7e8d05ebceac919ec64e1df3e8da1010f2ccd16c
Your Name <akbarhusainkadiwala32@gmail.com>
2024-04-12 17:21:49 +0530

    user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,27 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User} from "../models/user.model.js"
 
-const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message:"Backend Project"
+const registerUser = asyncHandler(async (req, res) => {
+    const { fullName, email, username, password } = req.body
+    // console.log("email :",email)
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+    //console.log(req.files);
 })
 
 
 
-export {registerUser}
+export { registerUser }
