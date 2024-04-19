858b047d99b0694ee6dae4ec1d087c2933d86c0a
Your Name <akbarhusainkadiwala32@gmail.com>
2024-04-19 18:00:02 +0530

    added content in comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -13,20 +13,101 @@ const getVideoComments = asyncHandler(async (req, res) => {
         throw new ApiError(400, "VideoId is Invalid")
     }
 
-
-
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
-})
+    const { videoId } = req.params
+    const { content } = req.body
+
+    if (!content) {
+        throw new ApiError(400, "Content is required")
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req.user?._id
+    })
+
+    if (!comment) {
+        throw new ApiError(500, "Failed to add comment please try again");
+    }
+
+    return res
+        .status(201)
+        .json(new ApiResponse(201, comment, "Comment added successfully"));
+});
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
-})
+    const { commentId } = req.params
+    const { content } = req.body
+
+    if (!content) {
+        throw new ApiError(400, "content is required");
+    }
+
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid commentId");
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if (!comment) {
+        throw new ApiError(400, "Comment not Found");
+    }
+
+    const updateComment = await Comment.findByIdAndUpdate(
+        comment?._id,
+        {
+            $set: {
+                content,
+            },
+        },
+        { new: true }
+    );
+
+    if (!updateComment) {
+        throw new ApiError(500, "Failed to edit comment please try again");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updateComment, "Comment updated successfully"));
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const { commentId } = req.params
+
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid commentId");
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if (!comment) {
+        throw new ApiError(400, "Comment not Found");
+    }
+
+    await Comment.findByIdAndDelete(commentId)
+
+    await Like.deleteMany({
+        comment: commentId,
+        likedBy: req.user
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, Tweet, "Comment deleted successfully"))
+
 })
 
 export {
